ba71c79518d2ec562bd67f54309ebbb53be50acd
MuhammadMomin095 <160387789+MuhammadMomin095@users.noreply.github.com>
2024-02-21 12:22:25 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+var members = ['Umer', 'Meer', 'Waleed', 'Hassan', 'Multazim'];
+for (var i = 0; i < members.length; i++) {
+    console.log(members[i]);
+}
---
+++ app.ts
@@ -0,0 +1,4 @@
+let members: string[] =['Umer','Meer','Waleed','Hassan','Multazim'];
+for(let i=0; i<members.length; i++) {
+    console.log(members[i]);
+}
